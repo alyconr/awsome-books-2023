e0d0078d248bf8a8df884bb5e72e813b8497b3b9
alyconr <alyconr@hotmail.com>
2023-06-13 13:19:46 -0500

    :sparkles: Add navigation menu feature

--- js/main.js
+++ js/main.js
@@ -2,4 +2,3 @@ import BookManager from './bookManager.js';
 
 const bookManager = new BookManager();
 
-document.getElementById('BookForm').addEventListener('submit', (event) => bookManager.addBook(event));
